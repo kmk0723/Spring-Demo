b5a65589eb7082007d03d6c3558965d05fc89f51
kmk0723 <manikarthikeya1@gmail.com>
2025-05-20 15:58:24 +0530

    Something file added

---
+++ Something.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Something {
+	public void build() {
+		System.out.println("Working....");
+	}
+}
